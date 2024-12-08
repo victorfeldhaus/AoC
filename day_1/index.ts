16ad55ca4fab715787c3caef7c8c3e1eec1e60aa
Victor Feldhaus <victorfeldhaus@Victors-MacBook-Air.local>
2024-12-08 16:51:02 -0300

    fix readfile types node

--- day_1/index.ts
+++ day_1/index.ts
@@ -1,15 +1,15 @@
-import "fs" from "node:fs";
+import fs from "node:fs";
+import path from "node:path";
 
 const main = () => {
-     console.log("Testando neovim caralho")
-    const list = fs.readFile(__dirname + "/list.txt", "utf8", (err, data) => {
-    if (err) {
-        console.erro(err);
-      return;
-    }
-    console.log(data)
-  })
-
+    console.log("Testing Neovim");
+    fs.readFile(path.join(__dirname, "list.txt"), "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(data);
+    });
 }
 
 main();
